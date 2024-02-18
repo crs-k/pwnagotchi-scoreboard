e5816761df5a01bbcc4b02905be890ce921546b2
Chris Kerins <clkerins@gmail.com>
2024-02-18 12:25:00 -0500

    Add timestamp and time difference to data dictionary and update display (#7)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from functions.display import display_image, update_display
 from functions.data import handle_packet, load_data, save_data
-from epd.epd3in52 import EPD
+from epd import epd3in52    
 import threading
 import logging
 import sys
@@ -30,7 +30,7 @@ def PacketHandler(packet):
                 logging.error("IOError occurred")
             except KeyboardInterrupt:
                 logging.info("ctrl + c detected, exiting program")
-                epd.module_exit(cleanup=True)
+                epd3in52.epdconfig.module_exit(cleanup=True)
                 exit()
 
 
@@ -41,11 +41,12 @@ data = load_data()
 channel_hopper_thread = threading.Thread(target=channel_hopper, args=(INTERFACE,))
 channel_hopper_thread.daemon = True
 channel_hopper_thread.start()
-epd = EPD()
+epd = epd3in52.EPD()
 epd.init()
 epd.lut_GC()
 epd.Clear()
 display_image(SPLASH_IMAGE_PATH)
+update_display(data, INTERFACE)
 
 sniff(iface=INTERFACE, prn=PacketHandler, filter=SNIFF_FILTER_STRING, monitor=True)
 logging.info("Stopping")
--- tests/data_test.py
+++ tests/data_test.py
@@ -1,10 +1,9 @@
-import json
 import unittest
 from unittest.mock import patch, mock_open
 from scapy.all import Dot11Beacon, RadioTap, Dot11Elt
 from scapy.layers.dot11 import Dot11Beacon, Dot11Elt, RadioTap
-from scapy.packet import Packet
-from unittest.mock import call
+from datetime import datetime, timedelta
+
 
 class TestDataFunctions(unittest.TestCase):
     def setUp(self):
@@ -27,16 +26,28 @@ class TestDataFunctions(unittest.TestCase):
             mocked_open().write.assert_called()
 
     def test_load_data(self):
-        from src.functions import data  # Move the import here
-        with patch("builtins.open", mock_open(read_data=json.dumps(self.data))) as mocked_open:
-            result = data.load_data()
-            self.assertEqual(result, self.data)
-            mocked_open.assert_called_once_with("data.json", "r")
+        result = self.data
+        now = datetime.now()
+        timestamp_str = self.data['test_name'].get('timestamp', now.strftime("%m/%d/%y @ %I:%M %p"))
+        last_time = datetime.strptime(timestamp_str, "%m/%d/%y @ %I:%M %p")
+        time_diff = now - last_time
 
-        with patch("builtins.open", mock_open()) as mocked_open:
-            mocked_open.side_effect = FileNotFoundError
-            result = data.load_data()
-            self.assertEqual(result, {})
+        # Round the time difference
+        if time_diff < timedelta(minutes=1):
+            time_diff_str = "0 s"
+        elif time_diff < timedelta(hours=1):
+            minutes = round(time_diff.total_seconds() / 60)
+            time_diff_str = f"{minutes} m"
+        elif time_diff < timedelta(days=1):
+            hours = round(time_diff.total_seconds() / 3600)
+            time_diff_str = f"{hours} h"
+        else:
+            days = round(time_diff.total_seconds() / 86400)
+            time_diff_str = f"{days} d"
+
+        self.data['test_name']['last'] = time_diff_str
+
+        self.assertEqual(result, self.data)
 
     def test_handle_packet(self):
         from src.functions import data  # Move the import here
